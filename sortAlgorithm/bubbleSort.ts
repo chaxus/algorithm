0c999dac18e54a2e44fe4ed6e6cbf8c1beedc8df
yuki <yangyuqing@192.168.1.9>
2021-12-30 09:18:56 +0800

    feat: 修改成ts

--- sortAlgorithm/bubbleSort.ts
+++ sortAlgorithm/bubbleSort.ts
@@ -1,5 +1,5 @@
 const arr = [1,4,54,5423,65,7,765,897,43,654,432,3,445,32,5,7,2,4,5,654,7,5]
-const bubbleSort = (arr) => {
+const bubbleSort = (arr:Array<number>) => {
     let size = arr.length
     for(let i = 0; i < size; i++){
         for(let j = 0; j < size; j++){
--- sortAlgorithm/insertionSort.ts
+++ sortAlgorithm/insertionSort.ts
@@ -1,6 +1,6 @@
-const arr = [1,4,54,5423,65,7,765,897,43,654,432,3,445,32,5,7,2,4,5,654,7,5]
+// const arr = [1,4,54,5423,65,7,765,897,43,654,432,3,445,32,5,7,2,4,5,654,7,5]
 
-const insertionSort = (arr) => {
+const insertionSort = (arr:Array<number>) => {
     const len = arr.length
     for(let i = 1; i < len; i++){
         let preIndex = i - 1
